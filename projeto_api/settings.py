33e1a4a20e5ec4bf8badb8118a2e271dcfc12eaf
portolucas <lucasportosd@gmail.com>
2021-01-29 01:26:42 -0300

    change version of psycopg2

--- projeto_api/settings.py
+++ projeto_api/settings.py
@@ -17,9 +17,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 # Quick-start development settings - unsuitable for production
